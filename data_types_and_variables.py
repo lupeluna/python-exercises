a1fc275b2f3cf72d410c0a1bdffc2385bee0fde3
Lupe Luna <luna.guadalupe898@gmail.com>
2021-04-06 21:40:44 -0500

    data types

---
+++ data_types_and_variables.py
@@ -0,0 +1,41 @@
+The_little_mermaid = 3
+Brother_Bear = 5
+Hercules = 1
+cost = 3
+total_days = The_little_mermaid + Brother_Bear + Hercules
+total_cost = total_days * cost
+print("The total cost for the movies is", total_cost, "dollars")
+
+
+
+
+# Suppose you are working as a contractor for 3 companies: Google, Amazon, Facebook,
+# they pay you a different rate per hour. Google pays 400/hr, Amazon 380/hr, and FB 350.
+# How much will you recive in pmt this week?  10 hrs FB, 6 hrs GGL, 4 hrs Amazon.
+
+google_hr = 400
+google_hrs_worked = 6
+google_pay = google_hr * google_hrs_worked
+
+amazon_hr = 380
+amazon_hrs_worked = 4
+amazon_pay = amazon_hr * amazon_hrs_worked
+
+fb_hr = 350
+fb_hrs_worked = 10
+fb_pay = fb_hr * fb_hrs_worked
+
+total_pay = google_pay + amazon_pay + fb_pay
+print("This week you will recieve", total_pay, "dollars.")
+
+
+
+# A student can be enrolled to a class only if the class is not full and the 
+#class schedule does not conflict with her current schedule.
+
+
+
+
+
+
+
