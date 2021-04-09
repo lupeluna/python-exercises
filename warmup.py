b365b83c1d88be7b3bd8831f1757aa29dd58c901
Lupe Luna <luna.guadalupe898@gmail.com>
2021-04-09 11:51:55 -0500

    warmups

---
+++ warmup.py
@@ -0,0 +1,34 @@
+# 1. Write the code to take a string and produce a dictionary out of that 
+# string such that the output looks like the following: Some thoughts:
+
+truck = "toyota tacoma"
+
+truck.split()
+
+make = truck.split()
+model = truck.split()[1]
+
+truck = {
+    "make": "toyota"
+    "model": "tacoma"
+}
+
+print(truck)
+
+
+# 2.  Write the code that takes a dictionary containing make/model for a 
+# vehicle and capitalizes the first character of the make and the model:
+
+def truck(n):
+    return n.title
+
+
+
+# 3. Create a list of 3 dictionaries where each dictionary represents a 
+# vehicle, as above Write the code that operates on that list of 
+# dictionaries in order to uppercase the entire make and model values.
+
+def dict(car1, car2, car3):
+    return car1.title, car2.title, car3.title
+
+
